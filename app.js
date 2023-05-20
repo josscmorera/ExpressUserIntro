2640b27c91a4a3d72d4b7b71fd430cfe35a694e7
josscmorera <63267272+josscmorera@users.noreply.github.com>
2023-05-20 13:20:25 -0400

    Create Routes

--- app.js
+++ app.js
@@ -30,6 +30,37 @@ app.get('/some-users/:countryName', (req, res) => {
     res.send(users);
 });
 
+// Post new user
+app.post('/new-user', (req, res) => {
+    console.log('New user received:', req.body);
+
+    if (!req.body || !req.body.gender || !req.body.name || !req.body.location || !req.body.email || !req.body.phone || !req.body.cell || !req.body.nat) {
+        return res.status(404).json({ error: true, message: "please fix your post data request" });
+    }
+
+    const newUser = {
+        gender: req.body.gender,
+        name: {
+            title: req.body.name.title,
+            first: req.body.name.first,
+            last: req.body.name.last,
+        },
+        location: {
+            city: req.body.location.city,
+            state: req.body.location.state,
+            country: req.body.location.country,
+            postcode: req.body.location.postcode,
+        },
+        email: req.body.email,
+        phone: req.body.phone,
+        cell: req.body.cell,
+        nat: req.body.nat,
+    };
+
+    usersList.push(newUser);
+    res.status(200).json({ message: "Success" });
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
